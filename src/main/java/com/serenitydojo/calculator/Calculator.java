9830fcec77c4cfe95362e76a66554933043e5683
pastrypossum <j.j@j.com>
2024-07-05 23:34:58 +0100

    Added string calculator

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,57 @@
 package com.serenitydojo.calculator;
 
+import org.apache.commons.lang3.StringUtils;
+import java.util.StringTokenizer;
+
 public class Calculator {
+
+    private int currentTotal = 0;
+    private String currentOperator = "";
+
     public int evaluate(String expression) {
-        return 0;
+
+        if(expression.isEmpty())
+            return 0;
+
+        StringTokenizer tokenizer = new StringTokenizer(expression, " ");
+        String firstToken = tokenizer.nextToken();
+        System.out.println("First token: " + firstToken);
+
+        if (StringUtils.isNumeric(firstToken)) {
+            currentTotal = Integer.parseInt(firstToken);
+        }
+
+        while (tokenizer.hasMoreTokens()) {
+
+            String token = tokenizer.nextToken();
+            System.out.println("Next token: " + token);
+
+            if (!StringUtils.isNumeric(token)) {
+                // operator
+                currentOperator = token;
+            }
+            else {
+                // number
+                int nextNumber = Integer.parseInt(token);
+                currentTotal = calculate(currentTotal, currentOperator, nextNumber);
+                System.out.println("Current total: " + currentTotal);
+            }
+        }
+        return currentTotal;
+    }
+
+    private int calculate (int total, String operator, int nextNumber) {
+
+        switch (operator) {
+            case "+":
+                return total + nextNumber;
+            case "-":
+                return total - nextNumber;
+            case "*":
+                return total * nextNumber;
+            default: {
+                throw new IllegalMathsOperatorException("Unknown operator: " + operator);
+            }
+        }
     }
 }
---
+++ src/main/java/com/serenitydojo/calculator/IllegalMathsOperatorException.java
@@ -0,0 +1,12 @@
+package com.serenitydojo.calculator;
+
+public class IllegalMathsOperatorException extends RuntimeException{
+
+    public IllegalMathsOperatorException(String message){
+        super();
+    }
+
+    public IllegalMathsOperatorException(String message, Throwable cause) {
+        super();
+    }
+}
